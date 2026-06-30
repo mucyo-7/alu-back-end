4ed9ce3bfdbd283a93c6f6aa447e5c51256f8fc3
mucyo-7 <d.mucyo1@alustudent.com>
2026-06-30 09:53:31 +0200

    Fix PEP8 issues and use int for employee_id

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
-"""Gather data from an API and display employee TODO list progress."""
+"""Script to gather data from an API for a given employee ID
+and display their TODO list progress.
+"""
 import requests
 import sys
 
 if __name__ == "__main__":
     employee_id = int(sys.argv[1])
-    base_url = "https://jsonplaceholder.typicode.com"
-    user_req = requests.get("{}/users/{}".format(base_url, employee_id))
-    todos_req = requests.get(
-        "{}/todos".format(base_url), params={"userId": employee_id}
+    url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
+    todo_url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
+        employee_id
     )
-    user = user_req.json()
-    todos = todos_req.json()
-    employee_name = user.get("name")
-    done_tasks = [t for t in todos if t.get("completed")]
-    total = len(todos)
-    done = len(done_tasks)
+    user_info = requests.get(url).json()
+    todo_info = requests.get(todo_url).json()
+    employee_name = user_info.get("name")
+    done_tasks = list(filter(lambda t: t.get("completed") is True, todo_info))
+    number_of_done_tasks = len(done_tasks)
+    total_tasks = len(todo_info)
     print("Employee {} is done with tasks({}/{}):".format(
-        employee_name, done, total))
+        employee_name, number_of_done_tasks, total_tasks))
     for task in done_tasks:
         print("\t {}".format(task.get("title")))
